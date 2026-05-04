0e4b6f877b16681b17470291af476c19ff91112a
yshevchukcloud <y.shevchuk@ortlyc.kyiv.ua>
2026-05-04 13:09:12 +0300

    v1

---
+++ unit_01/unit_01.js
@@ -0,0 +1,83 @@
+// Task 1.
+
+
+// Task 2.
+
+
+console.log('напишіть номер місяця тут');
+
+
+
+// Task 3.
+
+console.log('пишемо тут');
+
+
+// Task 4.
+
+
+
+
+// Task 5.
+
+
+
+// Task 6.
+
+
+
+// Task 7.
+
+
+
+// Task 8.
+
+
+// Task 9.
+
+
+
+
+// Task 10.
+
+
+// Task 11.
+
+
+
+
+
+// Task 12.
+
+
+
+// Task 13.
+
+
+
+// Task 14.
+
+let z1 = 6;
+let z2 = 3;
+
+// Task 15.
+
+
+
+// Task 16.
+
+
+
+
+// Task 17.
+
+
+// Task 18.
+
+
+
+// Task 19.
+
+
+
+// Task 20
---
+++ unit_02/unit_02.js
@@ -0,0 +1,127 @@
+// Task 1.
+
+// Task 2.
+
+
+// Task 3.
+
+
+// Task 4.
+
+// Task 5.
+
+
+// Task 6.
+
+
+// Task 7.
+
+
+// Task 8.
+
+function t8() {    
+    
+}
+
+document.querySelector('.b-8').onclick = t8;
+
+// Task 9.
+
+function t9() {
+    
+}
+
+document.querySelector('.b-9').onclick = t9;
+
+// Task 10.
+
+function t10() {
+    
+}
+
+document.querySelector('.b-10').onclick = t10;
+
+// Task 11.
+
+function t11() {
+    
+}
+
+
+document.querySelector('.b-11').onclick = t11;
+
+// Task 12.
+
+function t12() {
+    
+}
+
+
+document.querySelector('.b-12').onclick = t12;
+
+// Task 13.
+
+function t13() {
+    
+}
+
+document.querySelector('.b-13').onclick = t13;
+
+// Task 14.
+
+function t14() {
+    
+}
+
+
+document.querySelector('.b-14').onclick = t14;
+
+// Task 15.
+
+function t15() {
+
+}
+
+document.querySelector('.b-15').onclick = t15;
+
+// Task 16.
+
+function t16() {
+    
+}
+document.querySelector('.b-16').onclick = t16;
+
+// Task 17.
+
+
+function t17() {
+    
+}
+
+document.querySelector('.b-17').onclick = t17;
+
+// Task 18.
+
+
+function t18() {
+    
+}
+
+document.querySelector('.b-18').onclick = t18;
+
+// Task 19.
+
+function t19() {
+    
+}
+
+document.querySelector('.b-19').onclick = t19;
+
+// Task 20
+
+
+function t20() {
+    
+}
+
+document.querySelector('.b-20').onclick = t20;
